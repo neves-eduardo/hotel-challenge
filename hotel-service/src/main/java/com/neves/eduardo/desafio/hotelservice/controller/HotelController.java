d6fb39e5c76032dec17540d37333856e0400a3d4
neves.eduardo <eduardoneves858@gmail.com>
2024-05-17 01:09:38 -0300

    feature: implementing hotel delete and query by criteria

--- hotel-service/src/main/java/com/neves/eduardo/desafio/hotelservice/controller/HotelController.java
+++ hotel-service/src/main/java/com/neves/eduardo/desafio/hotelservice/controller/HotelController.java
@@ -1,14 +1,13 @@
 package com.neves.eduardo.desafio.hotelservice.controller;
 
 import com.neves.eduardo.desafio.hotelservice.dto.HotelDTO;
+import com.neves.eduardo.desafio.hotelservice.dto.HotelSearchCriteriaDTO;
+import com.neves.eduardo.desafio.hotelservice.entity.Hotel;
 import com.neves.eduardo.desafio.hotelservice.service.HotelService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.geo.Point;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-
-import static java.util.Objects.isNull;
 
 @RestController
 @RequestMapping("/hotels")
@@ -23,12 +22,27 @@ public class HotelController {
     }
 
     @GetMapping
-    public Flux<HotelDTO> getHotels() {
+    public Flux<HotelDTO> findAllHotels() {
         return hotelService.findAll();
     }
 
+    @GetMapping("/{id}")
+    public Mono<HotelDTO> findHotel(String id) {
+        return hotelService.find(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public Mono<Void> deleteHotel(@PathVariable String id) {
+        return hotelService.deleteHotel(id);
+    }
+
+    @GetMapping("/search")
+    public Flux<Hotel> findHotelsByCriteria(HotelSearchCriteriaDTO criteria) {
+        return hotelService.findHotelsByCriteria(criteria);
+    }
+
     @GetMapping("near")
-    public Flux<HotelDTO> getHotels(@RequestParam Double longitude, @RequestParam Double latitude, @RequestParam(required = false) Double maxDistance) {
+    public Flux<HotelDTO> findAllHotelsNear(@RequestParam Double longitude, @RequestParam Double latitude, @RequestParam(required = false) Double maxDistance) {
         return hotelService.findHotelNear(longitude, latitude, maxDistance);
     }
 
