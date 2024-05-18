fdee76abab0b3feb5d57a7f282ec3d93f3d7a111
neves.eduardo <eduardoneves858@gmail.com>
2024-05-18 02:49:48 -0300

    chore: adding logging

--- hotel-service/src/main/java/com/neves/eduardo/desafio/hotelservice/controller/HotelController.java
+++ hotel-service/src/main/java/com/neves/eduardo/desafio/hotelservice/controller/HotelController.java
@@ -6,6 +6,7 @@ import com.neves.eduardo.desafio.hotelservice.dto.HotelSearchCriteriaDTO;
 import com.neves.eduardo.desafio.hotelservice.entity.Hotel;
 import com.neves.eduardo.desafio.hotelservice.service.HotelService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -13,37 +14,44 @@ import reactor.core.publisher.Mono;
 @RestController
 @RequestMapping("/hotels")
 @RequiredArgsConstructor
+@Slf4j
 public class HotelController {
 
     private final HotelService hotelService;
 
     @PostMapping
     public Mono<HotelDTO> createHotel(@RequestBody HotelDTO hotel) {
+        log.info(String.format("[hotel-service] [controller] received hotel creation request. [%s]", hotel));
         return hotelService.create(hotel);
     }
 
     @GetMapping
     public Flux<HotelDTO> findAllHotels() {
+        log.info("[hotel-service] [controller] received request to find all hotels");
         return hotelService.findAll();
     }
 
     @GetMapping("/{id}")
     public Mono<HotelDTO> findHotel(String id) {
+        log.info(String.format("[hotel-service] [controller] received request to find hotel with id. [%s]", id));
         return hotelService.find(id);
     }
 
     @DeleteMapping("/{id}")
     public Mono<Void> deleteHotel(@PathVariable String id) {
+        log.info(String.format("[hotel-service] [controller] received request to delete  hotel with id. [%s]", id));
         return hotelService.deleteHotel(id);
     }
 
     @PostMapping("/search")
     public Flux<Hotel> findHotelsByCriteria(@RequestBody HotelSearchCriteriaDTO criteria) {
+        log.info(String.format("[hotel-service] [controller] received request to search hotels with criteria [%s].", criteria));
         return hotelService.findHotelsByCriteria(criteria);
     }
 
     @PostMapping("/{id}/reviews")
     public Mono<HotelDTO> createReview(@PathVariable String id, @RequestBody HotelReviewDTO reviewDTO) {
+        log.info(String.format("[hotel-service] [controller] received request to create a new review (body: [%s]) for hotel with id [%s].", reviewDTO, id));
         return hotelService.createHotelRating(id, reviewDTO);
     }
 
--- hotel-service/src/main/java/com/neves/eduardo/desafio/hotelservice/service/HotelService.java
+++ hotel-service/src/main/java/com/neves/eduardo/desafio/hotelservice/service/HotelService.java
@@ -10,6 +10,7 @@ import com.neves.eduardo.desafio.hotelservice.entity.HotelReview;
 import com.neves.eduardo.desafio.hotelservice.repository.CustomHotelRepository;
 import com.neves.eduardo.desafio.hotelservice.repository.HotelRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -20,6 +21,7 @@ import static org.springframework.util.CollectionUtils.isEmpty;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class HotelService {
 
     private final HotelRepository repository;
@@ -28,27 +30,37 @@ public class HotelService {
     private final CustomHotelRepository customHotelRepository;
 
     public Mono<HotelDTO> create(HotelDTO hotelDTO) {
+        log.info("[hotel-service] [service] creating a new hotel.");
         Hotel hotelEntity = hotelDTOtoHotelEntityConverter.map(hotelDTO);
+        log.info("[hotel-service] [service] saving a new hotel.");
+
         return repository.save(hotelEntity).map(hotelEntityToHotelDTO::map);
     }
 
     public Mono<HotelDTO> find(String id) {
+        log.info(String.format("[hotel-service] [service] finding hotel by id [%s].", id));
         return repository.findById(id).map(hotelEntityToHotelDTO::map);
     }
 
     public Flux<HotelDTO> findAll() {
+        log.info("[hotel-service] [service] finding all hotels.");
+
         return repository.findAll().map(hotelEntityToHotelDTO::map);
     }
 
     public Mono<Void> deleteHotel(String id) {
+        log.info(String.format("[hotel-service] [service] deleting hotel with id [%s].", id));
         return repository.deleteById(id);
     }
 
     public Flux<Hotel> findHotelsByCriteria(HotelSearchCriteriaDTO criteria) {
+        log.info(String.format("[hotel-service] [service] searching hotels with criteria [%s].", criteria));
         return customHotelRepository.searchHotels(criteria);
     }
 
     public Mono<HotelDTO> updateHotelAverageRating(String id) {
+        log.info(String.format("[hotel-service] [service] updating hotel with id [%s] average rating.", id));
+
         return calculateAverageRating(id).flatMap(averageRating ->
                 repository.findById(id).flatMap(hotel -> {
                     hotel.setAverageRating(averageRating);
@@ -57,6 +69,8 @@ public class HotelService {
     }
 
     public Mono<HotelDTO> createHotelRating(String id, HotelReviewDTO reviewDTO) {
+        log.info(String.format("[hotel-service] [service] creating hotel rating (body: [%s]) for hotel with id [%s].", reviewDTO, id));
+
         return repository.findById(id).flatMap(hotel -> {
                     List<HotelReview> reviews = hotel.getReviews();
 
