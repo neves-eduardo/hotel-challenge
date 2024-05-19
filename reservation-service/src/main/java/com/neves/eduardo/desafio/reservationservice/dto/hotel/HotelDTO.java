63133931081d865b10e904fe67740d67d182410c
neves.eduardo <eduardoneves858@gmail.com>
2024-05-19 00:27:42 -0300

    chore: commiting files for removal

--- reservation-service/src/main/java/com/neves/eduardo/desafio/reservationservice/dto/hotel/HotelDTO.java
+++ reservation-service/src/main/java/com/neves/eduardo/desafio/reservationservice/dto/hotel/HotelDTO.java
@@ -1,10 +1,5 @@
 package com.neves.eduardo.desafio.reservationservice.dto.hotel;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
