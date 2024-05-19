a73fde235d87dd62c323f69270af40fe8db955b9
neves.eduardo <eduardoneves858@gmail.com>
2024-05-19 16:16:29 -0300

    feature: implementing status update

--- reservation-service/src/main/java/com/neves/eduardo/desafio/reservationservice/consumer/ReservationNotificationConsumer.java
+++ reservation-service/src/main/java/com/neves/eduardo/desafio/reservationservice/consumer/ReservationNotificationConsumer.java
@@ -1,15 +1,29 @@
 package com.neves.eduardo.desafio.reservationservice.consumer;
 
 import com.neves.eduardo.desafio.reservationservice.dto.ReservationWebhookDTO;
+import com.neves.eduardo.desafio.reservationservice.service.ReservationService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
+@Slf4j
 public class ReservationNotificationConsumer {
+
+    private final ReservationService service;
 
     @KafkaListener(topics = "reservation-notification", groupId = "my-group")
     public void consume(ReservationWebhookDTO message) {
-        System.out.println("Message received: " + message);
+        service.findById(message.getId())
+                .flatMap(reservation -> {
+                    reservation.setStatus(message.getStatus());
+                    return service.update(reservation);
+                })
+                .doOnSuccess(updatedReservation -> log.info("Updated reservation: {}", updatedReservation))
+                .doOnError(error -> log.error("Failed to update reservation", error))
+                .subscribe();
     }
 
 }
